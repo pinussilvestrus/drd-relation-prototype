7a0e05614a2720d3100fe841850c80e548ae96d5
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-12-03 15:57:37 +0100

    feat(project): add relations view #1

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,9 @@
 const views = {
     drd: { selector: "#view-drd" },
     'dt-season': { selector: "#view-dt-season" },
-    'dt-holiday': { selector: "#view-dt-holiday" }
+    'dt-holiday': { selector: "#view-dt-holiday" },
+    'drd-relations': { selector: "#view-drd-relations" }
 }
-
 
 function switchView(viewId) {
 
@@ -11,6 +11,8 @@ function switchView(viewId) {
     const newView = views[viewId];
     const node = $(newView.selector);
     node.css('visibility', 'visible');
+
+    console.log(node);
 
     // make all others invisible
     Object.values(views).forEach(v => {
@@ -20,22 +22,34 @@ function switchView(viewId) {
     });
 }
 
+function toggleDRDHovers(val) {
+    const node1 = $('.connection-outline-permanent');
+
+    const node2 = $('.see-relations');
+
+    node1.css('visibility', val);
+    node2.css('visibility', val);
+}
+
 $(document).ready(function(){
     $('.to-dt-holiday').click(switchView.bind(this, 'dt-holiday'));
     $('.to-dt-season').click(switchView.bind(this, 'dt-season'));
     $('.to-drd').click(switchView.bind(this, 'drd'));
+    
+    $('.see-relations').click(function() {
+        switchView('drd-relations');
+        toggleDRDHovers('hidden');
+    });
 
     $('#view-drd').click(function(e) {
         if (e.target !== this) {
             return;
         }
 
-        $('.connection-outline-permanent').css('visibility', 'hidden');
-        $('.see-relations').css('visibility', 'hidden');
+        toggleDRDHovers('hidden');
     });
 
     $('.connection').click(function() {
-        $('.connection-outline-permanent').css('visibility', 'visible');
-        $('.see-relations').css('visibility', 'visible');
+        toggleDRDHovers('visible');
     });
 });
