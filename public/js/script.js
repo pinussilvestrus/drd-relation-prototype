241e42f95f3a8491798eadb310b60f31e15eba12
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-12-03 15:26:40 +0100

    feat(project): add relation button

--- public/js/script.js
+++ public/js/script.js
@@ -24,4 +24,18 @@ $(document).ready(function(){
     $('.to-dt-holiday').click(switchView.bind(this, 'dt-holiday'));
     $('.to-dt-season').click(switchView.bind(this, 'dt-season'));
     $('.to-drd').click(switchView.bind(this, 'drd'));
+
+    $('#view-drd').click(function(e) {
+        if (e.target !== this) {
+            return;
+        }
+
+        $('.connection-outline-permanent').css('visibility', 'hidden');
+        $('.see-relations').css('visibility', 'hidden');
+    });
+
+    $('.connection').click(function() {
+        $('.connection-outline-permanent').css('visibility', 'visible');
+        $('.see-relations').css('visibility', 'visible');
+    });
 });
